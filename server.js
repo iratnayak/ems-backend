75324935c2a06e67bf6af7a5f220ace1179cc6e4
iratnayak <iratnayake6@gmail.com>
2025-11-26 09:50:17 +0530

    Fixed CORS to allow Frontend URL

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 connectDB();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://ems-frontend-six-roan.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
